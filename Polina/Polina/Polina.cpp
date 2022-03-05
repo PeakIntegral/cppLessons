943c0db16ea7487cf7f600f9265a4a1163cb3c15
Polina <sysoeva_polina@mail.ru>
2022-03-05 21:17:35 +0300

    Create class

--- Polina/Polina/Polina.cpp
+++ Polina/Polina/Polina.cpp
@@ -11,22 +11,52 @@ using namespace std;
 // Тип ответа   Название(входные данные) {Действия}
 
 
-void createh(string pass) {
-    ifstream file(pass, fstream::in);
+class Hero {
+
+    // у каждого объекта есть переменная - член (можно померить)
+
+public:
     string name;
     int hp;
     int attack;
-    file >> name >> hp >> attack;
-    cout << "\n name:" << name;
-    cout << "\n hp:" << hp;
-    cout << "\n attack:" << attack;
+
+
+    // Методы = функция-член класса (что умеет)
+
+    bool islife() {
+        if (hp > 0) {
+            cout << "i am alive" << endl;
+            return true;
+
+        }
+    }
+
+
+};
+
+
+Hero createh(string pass) {
+    ifstream file(pass, fstream::in);
+    
+    Hero newplayer;
+
+
+    file >> newplayer.name >> newplayer.hp >> newplayer.attack;
+    cout << "\n name:" << newplayer.name;
+    cout << "\n hp:" << newplayer.hp;
+    cout << "\n attack:" << newplayer.attack;
     file.close();
+    return newplayer;
 }
 
 int main()
 {
     createh("player1.txt");
     createh("player2.txt");
+
+
+    Hero player1;
+
 
     
 
